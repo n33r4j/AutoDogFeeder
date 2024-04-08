3672c094a8de13f35d5f36faf72687e95dfa7e8d
n334j <n33r4j@users.noreply.github.com>
2024-04-08 02:51:08 +0000

    Add bare bones main console

--- src/adf_prototype/adf_prototype/MainConsole.py
+++ src/adf_prototype/adf_prototype/MainConsole.py
@@ -1,9 +1,34 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 
-from datetime import datetime
+
+class MainConsole(Node):
+    def __init__(self):
+        super().__init__('main_console')
+
+    def sub_callback(self, data):
+        pass
+
+    def timer_callback(self):
+        pass           
+        
+    def cleanup(self):
+        pass
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    main_console_node = MainConsole()
+    rclpy.spin(main_console_node)
+    
+    main_console_node.cleanup()
+    main_console_node.destroy_node()
+    
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
 
 
         
