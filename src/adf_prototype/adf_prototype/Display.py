5b869df0ace365ed6c4dfe01447157c49570246c
n33r4j <52103565+n33r4j@users.noreply.github.com>
2024-04-18 23:44:25 +0000

    FixUpdate display node errors

--- src/adf_prototype/adf_prototype/Display.py
+++ src/adf_prototype/adf_prototype/Display.py
@@ -1,7 +1,10 @@
 import rclpy
 from rclpy.node import Node
-from RPLCD import CharLCD
 from adf_interfaces.msg import LCD16x2
+
+## Only on PI
+# from RPLCD import CharLCD
+# from RPi.GPIO import GPIO
 
 
 C_UP_R = "\033[F" # – move cursor to the beginning of the previous line
@@ -24,15 +27,18 @@ class Display(Node):
 
         self.lcd_cols = 16
         self.lcd_rows = 2
+
+        self.isFirstTime = True
         
-        self.lcd = CharLCD(cols=self.lcd_cols, 
-                            rows=self.lcd_rows, 
-                            pin_rs=37, 
-                            pin_e=35, 
-                            pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
+        # self.lcd = CharLCD( numbering_mode=GPIO.BOARD,
+        #                     cols=self.lcd_cols, 
+        #                     rows=self.lcd_rows, 
+        #                     pin_rs=37, 
+        #                     pin_e=35, 
+        #                     pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
     
-        self.curr_text = ["", ""]
-        self.clear_line = " "*self.lcd_cols
+        self.curr_text = ["default", "text"]
+        self.clear_line = " "*(self.lcd_cols+2)
 
     def display_message_callback(self, data):
         self.curr_text[0] = data.l1
@@ -40,9 +46,14 @@ class Display(Node):
 
     def update_lcd(self):
         if MOCK:
-            print(4*(C_UP_R+clear_line), end="\r")
+            # To prevent overwriting previous commands.
+            if self.isFirstTime:
+                self.isFirstTime = False
+            else:
+                print(4*(C_UP_R+self.clear_line), end="\r")
+            
             print("-"*18)
-            print(f"|{line_1[i%2]: ^16}|\n|{line_2[i%2]: ^16}|")
+            print(f"|{self.curr_text[0]: ^16}|\n|{self.curr_text[1]: ^16}|")
             print("-"*18)
         else:
             self.lcd.clear()
