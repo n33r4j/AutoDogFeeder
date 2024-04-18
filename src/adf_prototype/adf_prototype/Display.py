e3add224aad9fbbdb93b82e2a0d52eb76678f751
n33r4j <52103565+n33r4j@users.noreply.github.com>
2024-04-18 07:14:10 +0000

    Add custom msg package with lcd msg

--- src/adf_prototype/adf_prototype/Display.py
+++ src/adf_prototype/adf_prototype/Display.py
@@ -2,6 +2,13 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from RPLCD import CharLCD
+from adf_interfaces.msg import LCD16x2
+
+
+C_UP_R = "\033[F" # – move cursor to the beginning of the previous line
+C_UP = "\033[A" # – move cursor up one line
+
+MOCK = True
 
 class Display(Node):
     def __init__(self):
@@ -13,13 +20,14 @@ class Display(Node):
                                                          10)
         self.lcd_cols = 16
         self.lcd_rows = 2
-        self.lcd = CharLCD(cols=self.lcd_cols, 
-                           rows=self.lcd_rows, 
-                           pin_rs=37, 
-                           pin_e=35, 
-                           pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
+        if not MOCK:
+            self.lcd = CharLCD(cols=self.lcd_cols, 
+                               rows=self.lcd_rows, 
+                               pin_rs=37, 
+                               pin_e=35, 
+                               pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
         
-        self.curr_text = ""
+        self.curr_text = ["", ""]
 
     def display_message_callback(self, data):
         self.curr_text = data.data
